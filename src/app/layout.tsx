c62937267c1cc12854a5b51623155cd5ffb9291a
Александр Трунов <aleksandrtrunov@MacBook-Pro-Aleksandr.local>
2025-06-23 15:04:37 +1200

    fix: seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import Layout from '@/components/layout/Layout';
+import type { Metadata } from "next";
+import "./globals.css";
+import Layout from "@/components/layout/Layout";
 
 export const metadata: Metadata = {
-	title: 'Kam fishing discovery',
-	description: 'РКЗ 37 -  экстримальные виды рыблки на Камчатке',
+  title: "Экстремальная рыбалка на Камчатке | RKZ37 Discovery",
+  description:
+    "Эксклюзивные рыболовные туры в труднодоступные места Камчатки. Ловля лосося, краба, морская рыбалка. Индивидуальные туры с опытными гидами.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ru">
-			<body>
-				<Layout>{children}</Layout>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="ru">
+      <body>
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
 }
--- src/components/modules/Main/Hero/Hero.tsx
+++ src/components/modules/Main/Hero/Hero.tsx
@@ -1,47 +1,48 @@
-'use client';
-import { useLang } from '@/hook/useLang';
-import Link from 'next/link';
+"use client";
+import { useLang } from "@/hook/useLang";
+import Link from "next/link";
 
-import { useEffect, useState, ReactNode } from 'react';
+import { useEffect, useState, ReactNode } from "react";
 
 const Hero = ({ children }: { children: ReactNode }) => {
-	const { lang, translations } = useLang();
-	const [videoReady, setVideoReady] = useState(false);
+  const { lang, translations } = useLang();
+  const [videoReady, setVideoReady] = useState(false);
 
-	useEffect(() => {
-		// Если вы будете использовать видео или что-то другое, зависящее от браузера
-		setVideoReady(true);
-	}, []);
+  useEffect(() => {
+    // Если вы будете использовать видео или что-то другое, зависящее от браузера
+    setVideoReady(true);
+  }, []);
 
-	return (
-		<main className="main">
-			{children}
-			<section className="video-bg">
-				{videoReady && (
-					<video
-						width="320"
-						height="240"
-						autoPlay
-						muted
-						loop
-						playsInline
-						controls={false}
-					>
-						<source src="/video/0828.mp4" type="video/mp4" />
-					</video>
-				)}
-				<div className="video-bg__content column">
-					<h1 className="main_title">{translations[lang].main.kamchatka}</h1>
-					<p className="main_discription">
-						{translations[lang].main.extreme_fishing}
-					</p>
-					<Link className="main__btn" href="/tours">
-						{translations[lang].main.btn_to_tours}
-					</Link>
-				</div>
-			</section>
-		</main>
-	);
+  return (
+    <main className="main">
+      {children}
+      <section className="video-bg">
+        {videoReady && (
+          <video
+            width="320"
+            height="240"
+            autoPlay
+            muted
+            loop
+            playsInline
+            controls={false}
+          >
+            <source src="/video/0828.mp4" type="video/mp4" />
+          </video>
+        )}
+        <div className="video-bg__content column">
+          <h1 className="main__titlSeo">{translations[lang].main.title}</h1>
+          <h2 className="main_title">{translations[lang].main.kamchatka}</h2>
+          <p className="main_discription">
+            {translations[lang].main.extreme_fishing}
+          </p>
+          <Link className="main__btn" href="/tours">
+            {translations[lang].main.btn_to_tours}
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Hero;
